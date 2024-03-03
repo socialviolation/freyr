82fe6f20a928ac583e5b5c6a6b3087fb8c92ac7b
Nick Freemantle <ng.freemantle@gmail.com>
2024-03-03 21:11:19 +1100

    return rbac

--- freyr/internal/controller/freyr_controller.go
+++ freyr/internal/controller/freyr_controller.go
@@ -49,6 +49,7 @@ type FreyrReconciler struct {
 //+kubebuilder:rbac:groups=freyr.fmtl.au,resources=freyrs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=freyr.fmtl.au,resources=freyrs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=freyr.fmtl.au,resources=freyrs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=*,resources=*,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
